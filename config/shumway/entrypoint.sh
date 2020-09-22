8d9f4ec3176fcfc3358d4d0e5081173e63378f29
Andrey Fadeev <me@ciiol.net>
2020-09-22 16:18:54 +0300

    Move application.properties into entrypoint.sh

--- config/shumway/entrypoint.sh
+++ config/shumway/entrypoint.sh
@@ -3,4 +3,5 @@ set -ue
 
 java \
     "-XX:OnOutOfMemoryError=kill %p" -XX:+HeapDumpOnOutOfMemoryError \
-    ${@}
+    ${@} \
+    --spring.config.additional-location=/vault/secrets/application.properties
