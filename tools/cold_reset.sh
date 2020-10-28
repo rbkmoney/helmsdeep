9d5e5b01653a2f55c044ef4d2cfded464f57ba06
Ivan Panteleev <amalgamm@users.noreply.github.com>
2020-10-28 20:10:40 +0300

    Fix deprecated chart repos & reset scripts (#102)
    * Fix deprecated chart repos & reset scripts
    
    * updated .lock via helmfile deps

--- tools/cold_reset.sh
+++ tools/cold_reset.sh
@@ -1,4 +1,5 @@
 minikube delete &&
+helm repo remove stable incubator bitnami hashicorp codecentric prometheus-community || echo "helm repos already deleted" &&
 minikube start --cpus=4 --memory="8g" --disk-size="40g" --addons="ingress" --driver="virtualbox" --network-plugin="cni" --extra-config="kubelet.network-plugin=cni" &&
 minikube ssh -- sudo mount bpffs -t bpf /sys/fs/bpf &&
 kubectl create -f https://raw.githubusercontent.com/cilium/cilium/v1.8/install/kubernetes/quick-install.yaml &&
