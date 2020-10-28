451ef1b0e3643112657936fdaed3b2a360e2bd1a
Ivan Panteleev <ispanteleev@gmail.com>
2020-10-28 19:37:59 +0300

    Fix deprecated chart repos & reset scripts

--- tools/quick_reset.sh
+++ tools/quick_reset.sh
@@ -1,6 +1,7 @@
 # Do not re-download images
 # authored by Dmitry Skokov <d.skokov@rbkmoney.com>
 helmfile delete \
+&& helm repo remove stable incubator bitnami hashicorp codecentric prometheus-community || echo "helm repos already deleted" \
 && kubectl delete deploy,rs,pvc,pv,svc,crd,ing,sts,job,cj,cm,secret,sa --all \
 && minikube ssh -- sudo rm -rf /tmp/hostpath-provisioner/default \
 && kubectl delete mutatingwebhookconfigurations,validatingwebhookconfigurations prometheus-prometheus-oper-admission || echo "prometheus webhooks already deleted" \
