5f913a8b6fefe92cf4e0479e885b3609db9db439
Andrey Abramov <a.abramov@rbkmoney.com>
2020-10-29 18:55:26 +0300

    Revert quick_reset to master

--- tools/quick_reset.sh
+++ tools/quick_reset.sh
@@ -7,6 +7,5 @@ helmfile delete \
 && kubectl delete mutatingwebhookconfigurations,validatingwebhookconfigurations prometheus-prometheus-oper-admission || echo "prometheus webhooks already deleted" \
 && kubectl delete ns monitoring elastic-system || echo "namespaces not found" \
 && kubectl delete -f https://raw.githubusercontent.com/cilium/cilium/v1.8/install/kubernetes/quick-install.yaml || echo "cilium not found" \
-&& sleep 45 \
 && kubectl apply -f https://raw.githubusercontent.com/cilium/cilium/v1.8/install/kubernetes/quick-install.yaml \
 && kubectl apply -f config/vault/init-cm.yaml
