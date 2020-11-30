53ae52d017d4111cfca9483f2e8fb22092dbdd74
Dmitry Skokov <d.skokov@rbkmoney.com>
2020-11-30 14:05:11 +0300

    move shumway

--- config/shumway/entrypoint.sh
+++ config/shumway/entrypoint.sh
@@ -3,5 +3,10 @@ set -ue
 
 java \
     "-XX:OnOutOfMemoryError=kill %p" -XX:+HeapDumpOnOutOfMemoryError \
+    -jar \
+    /opt/shumway/shumway.jar \
+    --logging.config=/opt/shumway/logback.xml \
+    --spring.flyway.table=schema_version \
+    --spring.flyway.schemas=shm \
     ${@} \
     --spring.config.additional-location=/vault/secrets/application.properties
