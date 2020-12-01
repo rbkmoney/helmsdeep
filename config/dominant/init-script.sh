59b5c8d5580690a2fdd7985420c54ac839ce77b3
Dmitry Skokov <d.skokov@rbkmoney.com>
2020-12-01 04:40:04 +0300

    moket url in dominanta fix

--- config/dominant/init-script.sh
+++ config/dominant/init-script.sh
@@ -270,7 +270,7 @@ FIXTURE=$(cat <<END
         "data": {
             "name": "Mocket Inspector Proxy",
             "description": "Mocked inspector proxy for integration test purposes",
-            "url": "http://proxy-mocket-inspector-api:8022/proxy/mocket/inspector",
+            "url": "http://proxy-mocket-inspector:8022/proxy/mocket/inspector",
             "options": {"risk_score": "high"}
         }
     }}}},
