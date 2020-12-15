a5eb02d5f8944f8352f282ef65ed4488052503af
Ivan Panteleev <ispanteleev@gmail.com>
2020-12-15 15:52:08 +0300

    sync

--- tools/cold_reset.sh
+++ tools/cold_reset.sh
@@ -3,7 +3,7 @@ export MINIKUBE_CPUS=${MINIKUBE_CPUS:-4}
 export MINIKUBE_DISK_SIZE=${MINIKUBE_DISK_SIZE:-40g}
 export MINIKUBE_DRIVER=${MINIKUBE_DRIVER:-virtualbox}
 minikube delete &&
-helm repo remove stable incubator bitnami hashicorp codecentric prometheus-community cilium || echo "helm repos already deleted" &&
+helm repo remove stable incubator bitnami hashicorp codecentric prometheus-community cilium rbkmoney || echo "helm repos already deleted" &&
 minikube start --addons="ingress" --network-plugin="cni" --extra-config="kubelet.network-plugin=cni" &&
 minikube ssh -- sudo mount bpffs -t bpf /sys/fs/bpf &&
 kubectl apply -f config/vault/init-cm.yaml
