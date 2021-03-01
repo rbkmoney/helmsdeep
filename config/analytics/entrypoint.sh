af9900b42877daf04a6d3668e276be070ada4877
karleowne <a.karlov@rbkmoney.com>
2021-03-01 14:43:54 +0700

    BJ-1035: add new service 'analytics'

--- config/analytics/entrypoint.sh
+++ config/analytics/entrypoint.sh
@@ -25,13 +25,13 @@ java \
     --spring.flyway.schemas=analytics \
     --postgres.db.schema=analytics \
     --flyway.schemas=analytics \
-    --columbus.url=http://columbus.default.svc.cluster.local:8022/repo \
+    --columbus.url=http://columbus:8022/repo \
     --columbus.networkTimeout=60000 \
-    --service.invoicing.url=http://hellgate.default.svc.cluster.local:8022/v1/processing/invoicing \
+    --service.invoicing.url=http://hellgate:8022/v1/processing/invoicing \
     --service.invoicing.networkTimeout=60000 \
-    --service.payouter.url=http://payouter.default.svc.cluster.local:8022/payout/management \
+    --service.payouter.url=http://payouter:8022/payout/management \
     --service.payouter.networkTimeout=60000 \
-    --service.dominant.url=http://dominant.default.svc.cluster.local:8022/v1/domain/repository_client \
+    --service.dominant.url=http://dominant:8022/v1/domain/repository_client \
     --service.dominant.networkTimeout=60000 \
     --service.dominant.scheduler.enabled=true \
     --service.dominant.scheduler.pollingDelay=10000 \
@@ -41,4 +41,3 @@ java \
     --logging.level.com.rbkmoney.analytics.listener.mapper.party.ContractorCreatedHandler=DEBUG \
     ${@} \
     --spring.config.additional-location=/vault/secrets/application.properties \
-
