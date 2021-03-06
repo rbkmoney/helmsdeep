f1a5e1b5e7dcaaa6aaf43295b8b67b8cc80dcfc2
karleowne <a.karlov@rbkmoney.com>
2021-03-06 16:06:47 +0700

    management.endpoint.prometheus.enabled=true

--- config/file-storage/entrypoint.sh
+++ config/file-storage/entrypoint.sh
@@ -6,6 +6,14 @@ java \
     -jar \
     /opt/file-storage/file-storage.jar \
     --logging.config=/opt/file-storage/logback.xml \
+    --management.security.flag=false \
+    --management.metrics.export.statsd.flavor=etsy \
+    --management.metrics.export.statsd.enabled=true \
+    --management.metrics.export.prometheus.enabled=true \
+    --management.endpoint.health.show-details=always \
+    --management.endpoint.metrics.enabled=true \
+    --management.endpoint.prometheus.enabled=true \
+    --management.endpoints.web.exposure.include=health,info,prometheus \
     --spring.servlet.multipart.max-file-size=10MB \
     --spring.servlet.multipart.max-request-size=10MB \
     --storage.endpoint=ceph:80 \
