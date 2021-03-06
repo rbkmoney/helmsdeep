1f734cb5cddb76e8fe04b9ef5caf804e73527a4a
karleowne <a.karlov@rbkmoney.com>
2021-03-06 15:56:43 +0700

    management.endpoint.prometheus.enabled=true

--- config/shumway/entrypoint.sh
+++ config/shumway/entrypoint.sh
@@ -6,6 +6,14 @@ java \
     -jar \
     /opt/shumway/shumway.jar \
     --logging.config=/opt/shumway/logback.xml \
+    --management.security.flag=false \
+    --management.metrics.export.statsd.flavor=etsy \
+    --management.metrics.export.statsd.enabled=true \
+    --management.metrics.export.prometheus.enabled=true \
+    --management.endpoint.health.show-details=always \
+    --management.endpoint.metrics.enabled=true \
+    --management.endpoint.prometheus.enabled=true \
+    --management.endpoints.web.exposure.include=health,info,prometheus \
     --spring.datasource.hikari.data-source-properties.prepareThreshold=0 \
     --spring.datasource.hikari.leak-detection-threshold=5300 \
     --spring.datasource.hikari.max-lifetime=300000 \
