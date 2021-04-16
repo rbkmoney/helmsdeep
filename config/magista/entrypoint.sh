279c8c5990071a0c637cc288ef9ab57f8c5a373a
Anatoly Karlov <a.karlov@rbkmoney.com>
2021-04-16 12:09:42 +0300

    Introduce magista (#169)
    * Introduce magista-kafka
    
    * Introduce magista-kafka
    
    * Introduce magista-kafka
    
    * Introduce mst
    
    * move kafka comment to the end of file
    
    * actualize magista
    
    * actualize magista
    
    * actualize mgista
    
    * management.endpoint.prometheus.enabled=true
    
    Co-authored-by: Dmitry Skokov <d.skokov@rbkmoney.com>
    Co-authored-by: vilorij <vilorij@ya.ru>

---
+++ config/magista/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -ue
+
+java \
+    "-XX:OnOutOfMemoryError=kill %p" -XX:+HeapDumpOnOutOfMemoryError \
+    -jar \
+    /opt/magista/magista.jar \
+    --logging.config=/opt/magista/logback.xml \
+    --management.security.flag=false \
+    --management.metrics.export.statsd.flavor=etsy \
+    --management.metrics.export.statsd.enabled=true \
+    --management.metrics.export.prometheus.enabled=true \
+    --management.endpoint.health.show-details=always \
+    --management.endpoint.metrics.enabled=true \
+    --management.endpoint.prometheus.enabled=true \
+    --management.endpoints.web.exposure.include=health,info,prometheus \
+    --spring.application.name=magista \
+    --spring.datasource.hikari.data-source-properties.prepareThreshold=0 \
+    --spring.datasource.hikari.leak-detection-threshold=5300 \
+    --spring.datasource.hikari.max-lifetime=300000 \
+    --spring.datasource.hikari.idle-timeout=30000 \
+    --spring.datasource.hikari.minimum-idle=2 \
+    --spring.datasource.hikari.maximum-pool-size=20 \
+    --flyway.schemas=mst \
+    --payouter.pooling.url=http://payouter:8022/repo \
+    --hellgate.url=http://hellgate:8022/v1/processing/partymgmt \
+    --hellgate.timeout=30000 \
+    --columbus.url=http://columbus:8022/repo \
+    --retry-policy.maxAttempts=-1 \
+    --kafka.bootstrap-servers=kafka:9092 \
+    --kafka.topics.invoicing=mg-events-invoice \
+    --kafka.client-id=magista \
+    --kafka.consumer.group-id=magista-invoicing-1 \
+    --kafka.consumer.concurrency=7 \
+    ${@} \
+    --spring.config.additional-location=/vault/secrets/application.properties \
