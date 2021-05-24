77d1af7d15542fec9119ba20a561bcdc7609fd4a
r.shaidullin <ndiezel0@gmail.com>
2021-05-24 19:16:20 +0300

    ED-159: Add bouncer and bouncer-policies

--- tools/cold_reset.sh
+++ tools/cold_reset.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export MINIKUBE_MEMORY=${MINIKUBE_MEMORY:-8000}
 export MINIKUBE_CPUS=${MINIKUBE_CPUS:-4}
 export MINIKUBE_DISK_SIZE=${MINIKUBE_DISK_SIZE:-40g}
@@ -5,5 +6,4 @@ export MINIKUBE_DRIVER=${MINIKUBE_DRIVER:-virtualbox}
 minikube delete &&
 helm repo remove stable incubator bitnami hashicorp codecentric prometheus-community cilium rbkmoney || echo "helm repos already deleted" &&
 minikube start --addons="ingress" --network-plugin="cni" --extra-config="kubelet.network-plugin=cni" &&
-minikube ssh -- sudo mount bpffs -t bpf /sys/fs/bpf &&
-kubectl apply -f config/vault/init-cm.yaml
+minikube ssh -- sudo mount bpffs -t bpf /sys/fs/bpf
