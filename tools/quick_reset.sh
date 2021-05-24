6224fddd8d9703b910f8d45f77cb374cc54ed91e
r.shaidullin <ndiezel0@gmail.com>
2021-05-24 19:16:20 +0300

    ED-159: Add bouncer and bouncer-policies

--- tools/quick_reset.sh
+++ tools/quick_reset.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Do not re-download images
 # authored by Dmitry Skokov <d.skokov@rbkmoney.com>
 helmfile delete \
@@ -5,4 +6,4 @@ helmfile delete \
 && kubectl delete deploy,rs,pvc,pv,svc,crd,ing,sts,job,cj,cm,secret,sa --all \
 && minikube ssh -- sudo rm -rf /tmp/hostpath-provisioner/default \
 && kubectl delete mutatingwebhookconfigurations,validatingwebhookconfigurations prometheus-prometheus-oper-admission || echo "prometheus webhooks already deleted" \
-&& kubectl delete ns monitoring elastic-system || echo "namespaces not found" \
+&& kubectl delete ns monitoring elastic-system || echo "namespaces not found"
