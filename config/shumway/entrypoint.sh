a56b4b6655961c1818e46ebb8213ba200e3e567c
r.shaidullin <ndiezel0@gmail.com>
2021-05-25 23:12:16 +0300

    ED-159: shamway fix

--- config/shumway/entrypoint.sh
+++ config/shumway/entrypoint.sh
@@ -22,4 +22,5 @@ java \
     --spring.datasource.hikari.minimum-idle=2 \
     --spring.datasource.hikari.maximum-pool-size=20 \
     ${@} \
-    --spring.config.additional-location=/vault/secrets/application.properties
+    --spring.config.additional-location=/vault/secrets/application.properties \
+    --spring.flyway.schemas=shm
