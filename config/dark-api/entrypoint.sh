93ca1015fc1206d7eddf55f9ea0ff7364cda130b
Dmitry Skokov <d.skokov@rbkmoney.com>
2021-07-08 03:54:38 +0300

    dark-api add

---
+++ config/dark-api/entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -ue
+
+java \
+    "-XX:OnOutOfMemoryError=kill %p" -XX:+HeapDumpOnOutOfMemoryError \
+    -jar \
+    /opt/dark-api/dark-api.jar \
+    --logging.config=/opt/dark-api/logback.xml \
+    --management.security.flag=false \
+    --management.metrics.export.statsd.flavor=etsy \
+    --management.metrics.export.statsd.enabled=true \
+    --management.metrics.export.prometheus.enabled=true \
+    --management.endpoint.health.show-details=always \
+    --management.endpoint.metrics.enabled=true \
+    --management.endpoint.prometheus.enabled=true \
+    --management.endpoints.web.exposure.include=health,info,prometheus \
+    --magista.client.adapter.url=http://magista:8022/v2/stat \
+    --magista.client.adapter.networkTimeout=90000 \
+    --claimmanagement.client.adapter.url=http://claim-management:8022/v1/cm \
+    --claimmanagement.client.adapter.networkTimeout=30000 \
+    --conversations.url=http://messages:8022/v1/messages \
+    --conversations.networkTimeout=30000 \
+    --questionary-aggr-proxy.url=http://questionary-aggr-proxy:8022/v1/questionary/proxy \
+    --questionary-aggr-prox.networkTimeout=30000 \
+    --questionary.url=http://questionary:8022/v1/questionary \
+    --questionary.networkTimeout=30000 \
+    --filestorage.client.adapter.url=http://file-storage:8022/file_storage \
+    --filestorage.client.adapter.networkTimeout=30000 \
+    --partyManagement.url=http://hellgate:8022/v1/processing/partymgmt \
+    --partyManagement.networkTimeout=30000 \
+    --cabi.url=http://cabi:8022/v1/cabi \
+    --cabi.networkTimeout=30000 \
+    --dominant.url=http://dominant:8022/v1/domain/repository \
+    --dominant.networkTimeout=30000 \
+    --dudoser.url=http://dudoser:8022/dudos \
+    --dudoser.networkTimeout=30000 \
+    --keycloak.realm-public-key.file-path="/var/lib/dark-api/keys/keycloak/keycloak.pubkey.pem" \
+    --keycloak.realm=external \
+    --keycloak.resource=common-api \
+    --server.servlet.context-path=/dark-api/v1 \
+    ${@} \
+    --spring.config.additional-location=/vault/secrets/application.properties \
