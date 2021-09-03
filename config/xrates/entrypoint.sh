cee683d4dfffa5d2d26aea4af0773bb124b40d21
Pospolita Nikita <nikita7asics@gmail.com>
2021-09-03 15:35:26 +0300

    Added xrates service (#205)
    * Added xrates service
    
    * adjust annotations and typofix
    
    * store xrates secret in dev vault
    
    * disable xrates deploy
    
    Co-authored-by: Dmitry Skokov <d.skokov@rbkmoney.com>
    Co-authored-by: vilorij <vilorij@ya.ru>

---
+++ config/xrates/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -ue
+
+java \
+    "-XX:OnOutOfMemoryError=kill %p" -XX:+HeapDumpOnOutOfMemoryError \
+    -jar \
+    /opt/xrates/xrates.jar \
+    --logging.config=/opt/xrates/logback.xml \
+    --management.security.enabled=false \
+    --management.security.flag=false \
+    --management.metrics.export.statsd.flavor=etsy \
+    --management.metrics.export.statsd.enabled=true \
+    --management.metrics.export.prometheus.enabled=true \
+    --management.endpoint.health.show-details=always \
+    --management.endpoint.metrics.enabled=true \
+    --management.endpoint.prometheus.enabled=true \
+    --management.endpoints.web.exposure.include=health,info,prometheus \
+    --spring.application.name=xrates \
+    --service.mg.automaton.url=http://machinegun:8022/v1/automaton \
+    --service.mg.automaton.namespace=xrates \
+    --service.mg.eventSink.url=http://machinegun:8022/v1/event_sink
+    --service.mg.eventSink.sinkId=xrates \
+    --sources.needInitialize=true \
+    ${@} \
+    --spring.config.additional-location=/vault/secrets/application.properties \
+    --sources.provider.secrets.file.path=/vault/secrets/secret.file \
