6afbcd18c60381e0c97436b8a62535ec32e7deb8
Gregory <32060161+ggmaleva@users.noreply.github.com>
2021-09-24 11:34:05 +0200

    add fraudbusters-warehouse (#230)
    * add fraudbusters-warehouse
    
    * add fraudbusters-warehouse in release

---
+++ config/fraudbusters-warehouse/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -ue
+
+java \
+    "-XX:OnOutOfMemoryError=kill %p" -XX:+HeapDumpOnOutOfMemoryError \
+    -jar \
+    /opt/fraudbusters-warehouse/fraudbusters-warehouse.jar \
+    --logging.config=/opt/fraudbusters-warehouse/logback.xml \
+    --management.security.enabled=false \
+    --management.endpoint.metrics.enabled=true \
+    --management.metrics.export.statsd.flavor=etsy \
+    --management.metrics.export.statsd.enabled=true \
+    --management.metrics.export.prometheus.enabled=true \
+    --management.endpoint.prometheus.enabled=true \
+    --management.endpoint.health.show-details=always \
+    --management.endpoints.web.exposure.include=health,info,prometheus \
+    ${@}
+    --spring.config.additional-location=/var/lib/fraudbusters-warehouse/additional.ch.properties
+
