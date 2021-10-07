9dcccbd34f45079cc7ec1e38c793ebd219f97a20
ggmaleva <griwania@gmail.com>
2021-10-07 16:59:17 +0300

    update wb-list-manager

--- config/wb-list-manager/entrypoint.sh
+++ config/wb-list-manager/entrypoint.sh
@@ -6,11 +6,16 @@ java \
     -jar \
     /opt/wb-list-manager/wb-list-manager.jar \
     --logging.config=/opt/wb-list-manager/logback.xml \
-    --logging.file=/var/log/wb-list-manager/wb-list-manager.json \
     --management.security.enabled=false \
     --riak.address=riak \
     --riak.port=8087 \
-    --kafka.bootstrap.servers=kafka:9092 \
+    --riak.bucket=wblist \
+    --retry.timeout=2000 \
+    --retry.max.attempts=3 \
+    --kafka.bootstrap-servers=kafka:9092 \
+    --kafka.wblist.topic.command=wb-list-command \
+    --kafka.wblist.topic.event.sink=wb-list-event-sink \
+    --kafka.ssl.enable=false \
     --management.metrics.export.statsd.enabled=false \
     ${@}
 
