c9efe451eb8bc3b913b45cf5695a7b14bc397992
Sergey Yelin <elinsn@gmail.com>
2022-01-20 14:57:08 +0300

    Fix proxy mocket inspector

--- config/proxy-mocket-inspector/entrypoint.sh
+++ config/proxy-mocket-inspector/entrypoint.sh
@@ -5,5 +5,6 @@ java \
     "-XX:OnOutOfMemoryError=kill %p" -XX:+HeapDumpOnOutOfMemoryError \
     -jar \
     /opt/proxy-mocket-inspector/proxy-mocket-inspector.jar \
+    --logging.config=/opt/proxy-mocket-inspector/logback.xml \
     --server.port=8022 \
     ${@}
