24dc7781f68a70f03d498088f4ca904153d788d2
Alexander Miroshnichenko <a.miroshnichenko@rbk.money>
2022-01-24 17:15:29 +0000

    clean infra services (#256)

--- tools/cold_reset.sh
+++ tools/cold_reset.sh
@@ -4,6 +4,6 @@ export MINIKUBE_CPUS=${MINIKUBE_CPUS:-5}
 export MINIKUBE_DISK_SIZE=${MINIKUBE_DISK_SIZE:-61g}
 export MINIKUBE_DRIVER=${MINIKUBE_DRIVER:-virtualbox}
 minikube delete &&
-helm repo remove stable incubator bitnami hashicorp codecentric prometheus-community cilium rbkmoney || echo "helm repos already deleted" &&
+helm repo remove stable incubator bitnami hashicorp codecentric prometheus-community rbkmoney || echo "helm repos already deleted" &&
 minikube start --addons="ingress" --network-plugin="cni" --extra-config="kubelet.network-plugin=cni" --kubernetes-version=v1.20.0 &&
 minikube ssh -- sudo mount bpffs -t bpf /sys/fs/bpf
